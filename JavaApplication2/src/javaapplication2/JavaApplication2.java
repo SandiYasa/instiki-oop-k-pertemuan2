978148a2fb4b161da037c006e0bb31ce2fb897d3
I Kadek Sandi Yasa <sandiyasa966@gmail.com>
2025-03-15 12:05:09 +0800

    Project pertemuan 2

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Sandi Yasa";
+        String NIM = "2301010070";
+        double IPK = 4.0;
+        System.out.print(nama);
+        System.out.println(NIM);
+        
+        System.out.printf("Nama Saya %s dengan NIM %s dengan IPK %.2f", nama,NIM,IPK);
+    }
+    
+}
